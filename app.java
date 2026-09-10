557777bee5808083c884dfe304967e300a9e1a5c
saiprasanna-e <saiprasannaeyyapu@gmail.com>
2026-09-10 08:41:05 +0530

    Update app.java

--- app.java
+++ app.java
@@ -5,6 +5,6 @@ def calculate(num1, num2):
    else:
        return num1 + num2
 # Example usage
-number1 = 20
-number2 = 30
-print("The result is", calculate(number1, number2))
+
+   remove 3 lines
+   
